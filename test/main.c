4667cb35977247d0ae30bcaebad3d688cb28cff1
Nicolas Leclerc <nicolas@coqsenpate.com>
2014-04-24 19:51:08 +0200

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Nicolas on 24/04/2014.
+//  Copyright (c) 2014 spirotron. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
